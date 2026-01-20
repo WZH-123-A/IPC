b2a09dc9d3be0a137106746b42c73651adf36c08
wuzhenhao <2818108279@qq.com>
2026-01-20 11:02:09 +0800

    [改动：修改编译报错]

--- server/ipc/src/main/java/com/ccs/ipc/IpcApplication.java
+++ server/ipc/src/main/java/com/ccs/ipc/IpcApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.ccs.ipc.mapper") //扫描mapper接口
 public class IpcApplication {
 
     public static void main(String[] args) {
